548d71a2960781f9f95f3d352b694a81672c2b0c
cReed2008InClass <colby.reed@stu.daviess.kyschools.us>
2025-09-02 13:22:56 -0500

    Death Screen

--- scenes/killzone.gd
+++ scenes/killzone.gd
@@ -1,11 +1,12 @@
 extends Area2D
 
-@onready var timer: Timer = $Timer
+#@onready var timer: Timer = $Timer
 
 func _on_body_entered(body: Node2D) -> void:
-	print("You Died!")
-	timer.start()
+	#print("You Died!")
+	#timer.start()
+	get_tree().change_scene_to_file("res://death_screen.tscn")
 	
 
-func _on_timer_timeout() -> void:
-	get_tree().reload_current_scene()
+#func _on_timer_timeout() -> void:
+	#get_tree().reload_current_scene()
