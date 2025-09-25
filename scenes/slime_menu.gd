1289810294c3885e3ac767b15c400480882a2d27
jDaugherty08 <joseph.daugherty@stu.daviess.kyschools.us>
2025-09-25 12:58:11 -0500

    fixed collision between the player and slimes

--- scenes/slime_menu.gd
+++ scenes/slime_menu.gd
@@ -4,20 +4,16 @@ var checkpoint_manager
 var player
 var slime
 
-
-
-
 func _ready() -> void:
 	
-	checkpoint_manager = get_node("../../../../CheckpointManager")
-	player = get_node("../../../../Player")
+	checkpoint_manager = get_node("../../../../../CheckpointManager")
+	player = get_node("../../../../../Player")
 	slime = get_node("../../..")
 
 func _on_resume_pressed() -> void:
 	slime.visible = false
 	self.visible = false
 	Engine.time_scale = 1
-
 
 func _on_dye_pressed() -> void:
 	enemyKillPlayer()
