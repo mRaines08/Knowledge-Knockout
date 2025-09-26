eafcdf500a396e44213faab605729c0a34003daa
cReed2008InClass <colby.reed@stu.daviess.kyschools.us>
2025-09-26 13:32:43 -0500

    Bottle Counter

---
+++ scripts/end_of_level_1.gd
@@ -0,0 +1,20 @@
+extends Node2D
+@onready var test: Label = $Test
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	$Test.text = "Knowledge Bottles: " + str(GlobalScript.count) + "/2"
+
+
+func _on_back_to_menu_pressed() -> void:
+	GlobalScript.count = 0
+	get_tree().change_scene_to_file("res://scenes/level_screen.tscn")
+
+
+func _on_next_level_pressed() -> void:
+	GlobalScript.count = 0
+	get_tree().change_scene_to_file("res://scenes/level_1.tscn")
--- scripts/knowledge_bottle.gd
+++ scripts/knowledge_bottle.gd
@@ -1,11 +1,13 @@
 extends Area2D
 
-@onready var score_manager: Node = %"Score Manager"
-@onready var player_score: Label = $"../Player/Player Score"
-@onready var bottle_score: Label = $"../Score Manager/Bottle Score"
+#@onready var score_manager: Node = %"Score Manager"
+#@onready var player_score: Label = $"../Player/Player Score"
+#@onready var bottle_score: Label = $"../Score Manager/Bottle Score"
 
 func _on_body_entered(body: Node2D) -> void:
 	queue_free()
-	score_manager.add_point()
+	#score_manager.add_point()
+	GlobalScript.count += 1
+	#GlobalScript.test.text = str(GlobalScript.count)
 	#player_score.text = bottle_score.text
 	
