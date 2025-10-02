a4cb14870386af4af4d0090976a7a57170b2360e
MalloryRainesInClass <mallory.raines@stu.daviess.kyschools.us>
2025-10-02 12:04:58 -0500

    correct math question

---
+++ scenes/slime_menu.gd
@@ -0,0 +1,26 @@
+extends Control
+
+var checkpoint_manager
+var player
+var slime
+#var fstSlime
+
+func _ready() -> void:
+	
+	checkpoint_manager = get_node("../../../../../CheckpointManager")
+	player = get_node("../../../../../Player")
+	slime = get_node("../../..")
+	#fstSlime = get_node("../../../../FastSlime")
+
+func _on_resume_pressed() -> void:
+	slime.visible = false
+	self.visible = false
+	Engine.time_scale = 1
+
+func _on_dye_pressed() -> void:
+	enemyKillPlayer()
+	self.visible = false
+	Engine.time_scale = 1
+
+func enemyKillPlayer():
+	player.position = checkpoint_manager.last_location
