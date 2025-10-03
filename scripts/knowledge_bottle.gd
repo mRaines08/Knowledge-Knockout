3b7c885785f3f06c0c3d983222977e4a75ae8ff7
cReed2008InClass <colby.reed@stu.daviess.kyschools.us>
2025-10-03 12:35:45 -0500

    Soundz

--- scripts/knowledge_bottle.gd
+++ scripts/knowledge_bottle.gd
@@ -1,13 +1,15 @@
 extends Area2D
+@onready var pick_up: AnimationPlayer = $PickUp
 
 #@onready var score_manager: Node = %"Score Manager"
 #@onready var player_score: Label = $"../Player/Player Score"
 #@onready var bottle_score: Label = $"../Score Manager/Bottle Score"
 
 func _on_body_entered(body: Node2D) -> void:
-	queue_free()
+	if body.is_in_group("Player"):
+		pick_up.play("Grab")
 	#score_manager.add_point()
-	GlobalScript.count += 1
+		GlobalScript.count += 1
 	#GlobalScript.test.text = str(GlobalScript.count)
 	#player_score.text = bottle_score.text
 	
